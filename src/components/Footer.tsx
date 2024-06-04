ab4836d9edf5a3810893e9aab631a1a0b0848b1a
Edgar Espinoza <edgarespinoza7@gmail.com>
2024-06-04 21:32:11 +0200

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='mt-auto border-t-2 text-center py-6 px-7 textz'>
+      <small>&copy; 2024. All rights reserved.</small>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const navLinks = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/posts",
+    label: "Posts",
+  },
+];
+
+const Header = () => {
+
+  const pathname = usePathname();
+
+  return (
+    <header className="flex flex-row justify-between items-center py-4 px-7 border-b-2">
+      <div>
+        <Link href="/">
+        <Image
+          src="https://bytegrad.com/course-assets/youtube/example-logo.png"
+          className="w-[35] h-[35]"
+          width={35}
+          height={35}
+          alt="Logo"
+        />
+        </Link>
+        
+      </div>
+      <div>
+        <ul className="flex gap-x-5 text-[14px]">
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <Link className={`${pathname === link.href ? "text-zinc-900" : "text-zinc-400"}`}
+              href={link.href}
+              >{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
